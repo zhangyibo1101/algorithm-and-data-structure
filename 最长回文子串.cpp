8579411271351d0b6ce9fa16e3b54c79578ec3e1
pure bo <2863510394@qq.com>
2022-03-02 19:22:31 +0800

    动规

--- 最长回文子串.cpp
+++ 最长回文子串.cpp
@@ -1,42 +1,67 @@
 #include <iostream>
 #include<string>
 using namespace std;
-string longestPalindrome(string s) {
-    int max = 1, start = 0;
-    for (int i = 1; i < s.length(); i++) {
-        int j = 1, num = 1;
-        while (j <= i && i + j < s.length()) {
-            if (s[i - j] == s[i + j]) {
-                num += 2;
-                j++;
-            }
-            else break;
+bool isexceed(vector<int>& v1) {
+    vector<int> v2;
+    int max = 2E31 - 1;
+    while (max) {
+        v2.push_back(max % 10);
+        max /= 10;
+    }
+    int i = 0;
+    while (i < 10) {
+        if (v2[9 - i] > v1[i]) {
+            return true;
         }
-        if (num > max) {
-            max = num;
-            start = i - j + 1;
+        else if (v2[9 - i] == v1[i]) {
+            i++;
         }
-        if (s[i] == s[i - 1]) {
-            int k = 1, num2 = 2;
-            while (k <= i - 1 && i + k < s.length()) {
-                if (s[i - 1 - k] == s[i + k]) {
-                    num2 += 2;
-                    k++;
-                }
-                else break;
-            }
-            if (num2 > max) {
-                max = num2;
-                start = i - k;
-            }
+        else {
+            return false;
         }
     }
-    return s.substr(start, max);
+    return false;
+
 }
+int link(vector<int>& v) {
+    int res = 0, index = 1;
+    for (int i = 0; i < v.size(); i++) {
+        res += index * v[i];
+        index *= 10;
+    }
+    return res;
+}
+int reverse(int x) {
+    if (x == 0 || x == -2e31)return 0;
+    vector<int> v;
+    if (x > 0) {
+        while (x) {
+            v.push_back(x % 10);
+            x /= 10;
+        }
+        if (v.size() == 10 && isexceed(v)) {
+            return 0;
+        }
+        else {
+            return link(v);
+        }
+    }
+    if (x < 0) {
+        x = -x;
+        while (x) {
+            v.push_back(x % 10);
+            x /= 10;
+        }if (v.size() == 10 && isexceed(v)) {
+            return 0;
+        }
+        else {
+            return -link(v);
+        }
+
+    }
 int main()
 {
-    // 请在此输入您的代码
-    cout << longestPalindrome("cbbd");
+    cout << reverse(123) << endl;
 
     system("pause");
     return 0;
